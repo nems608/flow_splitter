1b9c561bbe7146b98d23207093903787cb186589
Nic Mews-Schmuck <nems608@gmail.com>
2018-10-26 15:35:23 -0500

    Uncommend subprocess calls

--- flow_splitter.py
+++ flow_splitter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 import sys
 import subprocess
 import ipaddress
@@ -21,43 +21,43 @@ def parse_arguments():
 def enable_ip_fowarding():
     cmd = "sed -i 's/^.*net.ipv4.ip_forward.*$/net.ipv4.ip_forward=1/' /etc/sysctl.conf"
     print(cmd)
-    #subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
     cmd = 'sysctl -p /etc/sysctl.conf'
     print(cmd)
-    #subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
 
 def claim_ips(dist_dev, start_ip, netmask, end_ip):
     cmd = 'ip address flush {}'.format(dist_dev)
     print(cmd)
-    #subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
     print(cmd)
     cmd = 'ip link set dev {} up'.format(dist_dev)
-    #subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
     
     ip = ipaddress.ip_address(start_ip)
     while end_ip >= ip:
         cmd = 'ip address add {}/{} dev {}'.format(ip, netmask, dist_dev)
         print(cmd)
-        #subprocess.call(cmd, shell=True)
+        subprocess.call(cmd, shell=True)
         ip += 1
 
 def setup_snat(dist_dev, dest_net, start_ip, end_ip, out_dev):
     shared_opts='-t nat -I POSTROUTING 1 -o {} -p tcp -d {} -m state --state NEW -m statistic --mode nth'.format(dist_dev, dest_net)
     cmd = 'iptables -t nat -F'
     print(cmd)
-    #subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
   
     every = 1
     ip = ipaddress.ip_address(start_ip)
     while end_ip >= ip:
         cmd = 'iptables {} --every {} --packet 0 -j SNAT --to {}'.format(shared_opts, every, ip)
         print(cmd)
-        #subprocess.call(cmd, shell=True)
+        subprocess.call(cmd, shell=True)
         ip += 1
         every += 1
     cmd = 'iptables -t nat -A POSTROUTING -o {} -j MASQUERADE'.format(out_dev)
     print(cmd)
-    #subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
     if len(sys.argv) != 6:
